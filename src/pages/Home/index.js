a5c2ba12ff581a0da68e80157cb71ad503edeb6b
André Mendes <develmendes@gmail.com>
2022-07-11 20:28:03 -0300

    Configuração de rotas e style header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,28 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+import key from '../../apiKey';
+
 function Home(){
+
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=>{
+
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: api,
+          language: "pt-BR",
+          page: 1,
+        }
+      });
+
+      console.log(response.data.results);
+    }
+
+    loadFilmes();
+  }, [])
+
   return(
     <div>
       <h1>Bem vindo a home</h1>
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 
+import Header from './components/Header';
+
 function RoutesApp(){
   return(
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/filme/:id" element={ <Filme/> } />
-      </Routes>
+      <Header />
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/filme/:id" element={ <Filme/> } />
+        </Routes>
     </BrowserRouter>
   )
 }
