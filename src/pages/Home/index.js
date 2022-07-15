d53a51149c5443c6254c084efab01dea44046079
André Mendes <develmendes@gmail.com>
2022-07-15 15:53:16 -0300

    criação da home e style da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
-import key from '../../apiKey';
+import apiKey from '../../apiKey';
+import { Link } from 'react-router-dom'
+import './home.css';
+
 
 function Home(){
 
@@ -11,21 +14,31 @@ function Home(){
     async function loadFilmes(){
       const response = await api.get("movie/now_playing", {
         params: {
-          api_key: api,
+          api_key: apiKey,
           language: "pt-BR",
           page: 1,
         }
       });
 
-      console.log(response.data.results);
+      setFilmes(response.data.results.slice(0, 10));
     }
 
     loadFilmes();
   }, [])
 
   return(
-    <div>
-      <h1>Bem vindo a home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme)=> {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
